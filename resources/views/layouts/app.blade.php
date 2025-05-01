210984b61b99d4c0c8e400da5d8b89b1860fafc9
unknown <pujastodavid@gmail.com>
2025-05-01 14:52:43 +0700

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title') || {{ config('app.name') }}</title>
+    <meta content="Fahim Anzam Dip" name="author">
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!-- PWA  -->
+<meta name="theme-color" content="#6777ef"/>
+<link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
+<link rel="manifest" href="{{ asset('/manifest.json') }}">
+
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('images/favicon.png') }}">
+
+    @include('includes.main-css')
+</head>
+
+<body class="c-app">
+    @include('layouts.sidebar')
+
+    <div class="c-wrapper">
+        <header class="c-header c-header-light c-header-fixed">
+            @include('layouts.header')
+            <div class="c-subheader justify-content-between px-3">
+                @yield('breadcrumb')
+            </div>
+        </header>
+
+        <div class="c-body">
+            <main class="c-main">
+                @yield('content')
+            </main>
+        </div>
+
+        @include('layouts.footer')
+    </div>
+
+    @include('includes.main-js')
+
+    <script src="{{ asset('/sw.js') }}"></script>
+<script>
+   if ("serviceWorker" in navigator) {
+      // Register a service worker hosted at the root of the
+      // site using the default scope.
+      navigator.serviceWorker.register("/sw.js").then(
+      (registration) => {
+         console.log("Service worker registration succeeded:", registration);
+      },
+      (error) => {
+         console.error(`Service worker registration failed: ${error}`);
+      },
+    );
+  } else {
+     console.error("Service workers are not supported.");
+  }
+</script>
+</body>
+</html>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>Agrapana IT Solution © {{ date('Y') }} || Developed by <strong><a class="text-dark" target="_blank" href="#">David Pujasto</a></strong ></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">3.1</strong></div>
+</footer>
